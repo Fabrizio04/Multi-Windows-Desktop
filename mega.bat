380a87708992c7394a33b9266e05b8a97e94cc61
Fabrizio Amorelli <amorellif94@gmail.com>
2022-02-06 01:06:17 +0100

    Delete mega.bat

--- mega.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Multi-Windows by Fabrizio Amorelli
-color 9F
-if '%2'=='' (
-"%~dp0\mega\megadl.exe" --path %tmp% %1
-) else (
-"%~dp0\mega\megadl.exe" --proxy %2 --path %tmp% %1
-)
-exit
